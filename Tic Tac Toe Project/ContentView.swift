9fec174be4077fc4de183cc61b569dcb7bd5e6b5
amritaismypal <gudiyaapal@gmail.com>
2022-06-06 19:53:28 -0500

    Initial Commit

---
+++ Tic Tac Toe Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe Project
+//
+//  Created by Amrita Pal on 6/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Tac Toe Project/Tic_Tac_Toe_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_ProjectApp.swift
+//  Tic Tac Toe Project
+//
+//  Created by Amrita Pal on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
